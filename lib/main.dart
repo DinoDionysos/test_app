53fe70873b0e41b03e489834abb36fa4c1c82d13
Daniel Bachus <dani.accounts@mailbox.org>
2021-11-24 02:30:46 +0100

    main.dart: rename the _darkTheme

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      darkTheme: _darkTheme,
+      darkTheme: darkThemeSimple,
       home: MyHomePage(),
     );
   }
